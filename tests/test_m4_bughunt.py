f1e298bf682e553bd24079c3bfad55e0eda4d317
Luis Rosario <luisrosarioalers@gmail.com>
2026-05-28 15:50:36 -0500

    create m4 bughunt test and update m4 notes

---
+++ tests/test_m4_bughunt.py
@@ -0,0 +1,12 @@
+import pytest
+
+def average_rating(rating):
+    return sum(rating) / len(rating)
+
+@pytest.mark.parametrize("input_list, expected_output", [
+    ([4, 5, 3], 4.0),    
+    ([1, 2], 1.5),       
+    ([], None),          
+])
+def test_average_rating_edge(input_list, expected_output):
+    assert average_rating(input_list) == expected_output
